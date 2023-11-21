5cd5ef97389acab4ef14b2b061b66a9e4fb157b5
Jolexer <jolex.nerd@gmail.com>
2023-11-21 19:37:17 +0100

    Initial Commit

---
+++ the land of us/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  the land of us
+//
+//  Created by Jorik de boer on 21/11/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
